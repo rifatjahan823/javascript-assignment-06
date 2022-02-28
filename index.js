50cd73b31075b9fa7c9edf5433424b4100d7acec
rifatjahan823 <rifatjahan823@gmail.com>
2022-02-28 23:04:28 +0600

    add javascript

--- index.js
+++ index.js
@@ -11,21 +11,24 @@ const searchPhone = ()=>{
 //show the search result
 const showResult = resultValue=>{
     const showValue = resultValue.data
+    // <!-- showResul id search result -->
     const showResult = document.getElementById("showResult")
     showResult.innerHTML=""
-    const details = document.getElementById("details")
-    details.innerText=""
-    showValue.forEach(resultValues => {
+    //image details id for clean search result
+     const details = document.getElementById("details")
+     details.innerText=""
+     //create element
+       showValue.forEach(resultValues => {
        const div = document.createElement("div");
        div.innerHTML=`
-       <div onclick="showDetails('${resultValues.slug}')" class="card h-100 text-center p-3">
+       <div  class="card h-100 text-center p-3">
        <img  src="${resultValues.image}" class="card-img-top w-50 mx-auto" alt="...">
        <div class="card-body">
          <h5 class="card-title">${resultValues.phone_name}</h5>
          <p class="card-text">${resultValues.brand}</p>
        </div>
        <div>
-       <button>Details</button>
+       <button onclick="showDetails('${resultValues.slug}')">Details</button>
        </div>
      </div>
        `
@@ -49,12 +52,20 @@ const fullDetails = fullDetailsValue=>{
     //create element
         const div = document.createElement("div");
         div.innerHTML=`
-        <div class="card mx-auto text-center my-4 p-3" style="width: 18rem;">
+        <div class="card mx-auto text-center my-4 p-3" style="width: 30rem;">
         <img src="${detailsResult.image}" class="card-img-top w-50 mx-auto" alt="...">
         <div class="card-body">
           <p class="card-title">Release Date:${detailsResult.releaseDate?detailsResult.releaseDate:'no result found'}</p>
           <p class="card-text">storage:${detailsResult.mainFeatures.storage}</p>
-          <p>displaySize:${detailsResult.displaySize}</p>
+          <p>displaySize:${detailsResult.mainFeatures.displaySize}</p>
+          <p>displaySize:${detailsResult.mainFeatures.chipSet}</p>
+          <p>memory:${detailsResult.mainFeatures.memory}</p>
+          <p>sensors:${detailsResult.mainFeatures.sensors}</p>
+          <p>WLAN:${detailsResult.others.WLAN}</p>
+          <p>Bluetooth:${detailsResult.others.Bluetooth}</p>
+          <p>NFC:${detailsResult.others.NFC}</p>
+          <p>Radio:${detailsResult.others.Radio}</p>
+          <p>USB:${detailsResult.others.USB}</p>
           
         </div>
       </div>
