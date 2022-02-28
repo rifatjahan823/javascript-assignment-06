c57701392aaa1778a15b3369a97cdcfffef2e132
rifatjahan823 <rifatjahan823@gmail.com>
2022-03-01 04:49:58 +0600

    javascript slice add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const searchPhone = ()=>{
     const input = document.getElementById("input");
-    spiner ("block")
+    document.getElementById("image").style.display='block'
     inputValue = input.value;
     input.value=""
 
@@ -8,34 +8,36 @@ const searchPhone = ()=>{
     .then(res=>res.json())
     .then(data=>showResult(data))
 }
+searchPhone ()
 
 //show the search result
 const showResult = resultValue=>{
     const showValue = resultValue.data
+    const isStatus = resultValue.status
     // <!-- showResul id search result -->
     const showResult = document.getElementById("showResult")
     showResult.innerHTML=""
     //image details id for clean search result
-     const details = document.getElementById("details")
-     details.innerText=""
-     
-  if(!showValue){
-    document.getElementById("errorResult").style.display="block"
+    const details = document.getElementById("details")
+    details.innerText="" 
+
+  if(!showValue || isStatus== false){
+   document.getElementById("errorResult").style.display="block"
   }
   else{
     document.getElementById("errorResult").style.display="none"
          //create element
-         showValue.forEach(resultValues => {
+         showValue.slice(0,20).forEach(resultValues=> {
             const div = document.createElement("div");
             div.innerHTML=`
             <div  class="card h-100 text-center p-3">
             <img  src="${resultValues.image}" class="card-img-top w-50 mx-auto" alt="...">
             <div class="card-body">
-              <h5 class="card-title">${resultValues.phone_name}</h5>
-              <p class="card-text">${resultValues.brand}</p>
+              <h5 class="card-title ">${resultValues.phone_name}</h5>
+              <p class="card-text "><span style="font-weight:bold">Brand:</span>${resultValues.brand}</p>
             </div>
             <div>
-            <button onclick="showDetails('${resultValues.slug}')">Details</button>
+            <button class="btn btn-primary" onclick="showDetails('${resultValues.slug}')">Details</button>
             </div>
           </div>
             `
@@ -72,17 +74,17 @@ const fullDetails = fullDetailsValue=>{
         </div>
          <div class="col-12 col-lg-8 col-md-8">
          <div class="card-body">
-        <p class="card-title">Release Date:${detailsResult.releaseDate?detailsResult.releaseDate:'no result found'}</p>
-        <p class="card-text">storage:${detailsResult.mainFeatures.storage}</p>
-        <p>displaySize:${detailsResult.mainFeatures.displaySize}</p>
-        <p>displaySize:${detailsResult.mainFeatures.chipSet}</p>
-        <p>memory:${detailsResult.mainFeatures.memory}</p>
-        <p class=" text-danger">sensors:${detailsResult.mainFeatures.sensors}</p>
-        <p>WLAN:${detailsResult.others?.WLAN? detailsResult.others?.WLAN:'no result'}</p>
-        <p>Bluetooth:${detailsResult.others?.Bluetooth? detailsResult.others?.Bluetooth:'no result'}</p>
-        <p>NFC:${detailsResult.others?.NFC? detailsResult.others?.NFC:'no result'}</p>
-        <p>Radio:${detailsResult.others?.Radio? detailsResult.others?.Radio:'no result'}</p>
-        <p>USB:${detailsResult.others?.USB? detailsResult.others?.USB:'no result'}</p>
+        <p class="card-title"><span style="font-weight:bold">Release Date:</span>${detailsResult.releaseDate?detailsResult.releaseDate:'no result found'}</p>
+        <p class="card-text"><span style="font-weight:bold">Storage:</span>${detailsResult.mainFeatures.storage}</p>
+        <p><span style="font-weight:bold">DisplaySize:</span>${detailsResult.mainFeatures.displaySize}</p>
+        <p><span style="font-weight:bold">ChipSet:</span>${detailsResult.mainFeatures.chipSet}</p>
+        <p><span style="font-weight:bold">Memory:</span>${detailsResult.mainFeatures.memory}</p>
+        <p ><span style="font-weight:bold">Sensors:</span>${detailsResult.mainFeatures.sensors}</p>
+        <p><span style="font-weight:bold">WLAN:</span>${detailsResult.others?.WLAN? detailsResult.others?.WLAN:'no result'}</p>
+        <p><span style="font-weight:bold">Bluetooth:</span>${detailsResult.others?.Bluetooth? detailsResult.others?.Bluetooth:'no result'}</p>
+        <p><span style="font-weight:bold">NFC:</span>${detailsResult.others?.NFC? detailsResult.others?.NFC:'no result'}</p>
+        <p><span style="font-weight:bold">Radio:</sapn>${detailsResult.others?.Radio? detailsResult.others?.Radio:'no result'}</p>
+        <p><span style="font-weight:bold">USB:</span>${detailsResult.others?.USB? detailsResult.others?.USB:'no result'}</p>
       </div>
     </div>
     </div>
